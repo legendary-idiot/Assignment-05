a4158580e157837817a011e118c4bbaacdcc8cf8
Shafayath Jamil <shafayathj@gmail.com>
2024-12-09 11:54:22 +0600

    Toggle Button Improved, Donation Input Validity Optimized

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,16 +5,31 @@ const donationContainer = document.getElementById("donation-container");
 const historyBtn = document.getElementById("history-btn");
 
 // Toggle Donation and History Button
+function classModifier(
+  primaryContainer,
+  secondaryContainer,
+  primaryBtn,
+  secondaryBtn
+) {
+  // Container Toggle
+  primaryContainer.classList.remove("hidden");
+  secondaryContainer.classList.add("hidden");
+
+  // Button Background Color Toggle
+  primaryBtn.classList.add("bg-[#b4f461]", "text-[#111111]");
+  primaryBtn.classList.remove("border");
+  secondaryBtn.classList.remove("bg-[#b4f461]", "text-[#111111]");
+  secondaryBtn.classList.add("text-[#111111B2]", "border");
+}
+// Event Listener on the Buttons
 historyBtn.addEventListener("click", () => {
-  donationContainer.classList.add("hidden");
-  historyContainer.classList.remove("hidden");
+  classModifier(historyContainer, donationContainer, historyBtn, donationBtn);
 });
 donationBtn.addEventListener("click", () => {
-  historyContainer.classList.add("hidden");
-  donationContainer.classList.remove("hidden");
+  classModifier(donationContainer, historyContainer, donationBtn, historyBtn);
 });
 
-//Obtain Existing Balance
+// Obtain Existing Balance
 let currentTotalBalance = document.getElementById("account-balance");
 let currentNoakhaliBalance = document.getElementById("balance-noakhali");
 let currentFeniBalance = document.getElementById("balance-feni");
@@ -33,27 +48,35 @@ function numberChecker(testCase) {
 }
 
 const donateNowChoices = {
-  noakhali: ["donatedAmountNoakhali", "donate-noakhali-btn"],
-  feni: ["donatedAmountFeni", "donate-feni-btn"],
-  quota: ["donatedAmountQuota", "donate-quota-btn"],
+  noakhali: [
+    "donatedAmountNoakhali",
+    "donate-noakhali-btn",
+    currentNoakhaliBalance,
+  ],
+  feni: ["donatedAmountFeni", "donate-feni-btn", currentFeniBalance],
+  quota: ["donatedAmountQuota", "donate-quota-btn", currentQuotaBalance],
 };
 
 for (const donateNow in donateNowChoices) {
   const inputID = document.getElementById(donateNowChoices[donateNow][0]);
   const btnID = document.getElementById(donateNowChoices[donateNow][1]);
-  btnID.addEventListener("click", (e) => {
+  const selectedEvent = donateNowChoices[donateNow][2];
+  btnID.addEventListener("click", () => {
     const inputAmount = inputID.value;
     const isNumberValid = numberChecker(inputAmount);
 
     if (isNumberValid) {
       const donatedAmount = parseInt(inputAmount);
       const currentBalance = parseInt(currentTotalBalance.innerText);
+      const selectedEventBalance = parseInt(selectedEvent.innerText);
       if (donatedAmount > currentBalance) {
         alert("Sorry, You don't have sufficient balance!!");
       } else {
         const newBalance = currentBalance - donatedAmount;
         currentTotalBalance.innerText = newBalance;
+        selectedEvent.innerText = selectedEventBalance + donatedAmount;
         my_modal_5.showModal();
+        inputID.value = "";
       }
     }
   });
